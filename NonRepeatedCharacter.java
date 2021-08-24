70c3b1f46fcb7c05c2cc4a2b4b8547b22f9733e3
sravzsk <89446526+sravzsk@users.noreply.github.com>
2021-08-24 14:17:38 +0530

    Update NonRepeatedCharacter.java

--- NonRepeatedCharacter.java
+++ NonRepeatedCharacter.java
@@ -21,7 +21,7 @@ public class NonRepeatedCharacter {
 		
 		for(Entry<Character,Integer> entry:hasMap.entrySet()) {
 			if(entry.getValue()==1){
-					System.out.println("nonRepeatedCharacter-->"+entry.getKey());
+					System.out.println("nonRepeatedCharacter value->"+entry.getKey());
 					return;
 			}
 		}
